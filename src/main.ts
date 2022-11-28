31924712a04209a397d84d3528f463196427adc2
Filippe Rodrigues <filipperodrigues@rede.ulbra.br>
2022-11-28 15:30:48 -0300

    feat: Added PlugCheckoutFull Vue2 example

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import Vue from "vue";
+import App from "./App.vue";
+import {
+  applyPolyfills,
+  defineCustomElements,
+} from "@plug-checkout/core/loader";
+
+Vue.config.productionTip = false;
+
+Vue.config.ignoredElements = [/plug-\w*/];
+
+applyPolyfills().then(() => {
+  defineCustomElements();
+});
+
+new Vue({
+  render: (h) => h(App),
+}).$mount("#app");
